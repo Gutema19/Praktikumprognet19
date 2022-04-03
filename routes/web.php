772ed58ea7395b7b714c295d7879aaa10ac6d4f0
Herry Liukae <asunakirito211@gmail.com>
2022-04-04 07:39:36 +0800

    Revisi dikit modul 1

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'prevent-back-history'], function () {
+    Auth::routes(['verify' => true]);
+
     Route::get('/', function () {
         return view('welcome');
     })->middleware('guest')->name('landingpg');
@@ -59,7 +61,6 @@ Route::group(['middleware' => 'prevent-back-history'], function () {
     // Route untuk password
     Route::get('/passview', [passctrl::class, 'index'])->middleware('guest')->name('forgot.password');
 
-    Auth::routes(['verify' => true]);
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('auth')->name('homeuser');
 
